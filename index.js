624c41df5c00a0ec1658d7491c6594acf38191f3
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-05-01 13:45:00 +0530

    added index.js as scraper file

--- index.js
+++ index.js
@@ -1,79 +1,75 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
+const fs = require('fs');
 
-async function scrapeIndeed() {
+var jobWebsiteUrl = 'https://www.indeed.com/' //Url of website which I want to scraping by puppeteer
+
+const scrapeIndeed = async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
-    let status = await page.goto('https://www.indeed.com/');
+    let status = await page.goto(jobWebsiteUrl);
+    // verify the website url like it is exsit or not by status code;
     status = status.status();
     if (status != 404) {
         console.log(`Probably HTTP response status code 200 OK.`);
-    }else{
+    } else {
         return;
     }
 
-    // Enter search query
-    await page.type('#text-input-what', 'full stack developer');
+    await page.screenshot({ path: "example.png", fullPage: true });
+    await page.pdf({ path: 'demo.pdf', format: "A4" });
 
+    // Enter search query
+    await page.type('#text-input-what', 'software developer');
     // Enter location
     await page.type('#text-input-where', 'New Delhi, Delhi');
+    await page.click("[type=submit]"); //click of find Job button or It will return jobs after 5000ms automatically according to query;
+    await page.waitForTimeout(5000);
 
-    // Click "Find Jobs" button
-    await Promise.all([
-        page.waitForNavigation(),
-        page.click('.yosegi-InlineWhatWhere-primaryButton')
-    ]);
 
-    let jobCount = 0;
-    let jobs = [];
-
-    while (true) {
-        // Wait for search results to load
-        await page.waitForSelector('#mosaic-provider-jobcards');
-
-        // Extract job count on first page
-        // if (jobCount === 0) {
-        //     const jobCountText = await page.$$eval('#searchCountPages');
-        //     console.log("jobCountText", jobCountText);
-        //     jobCount = parseInt(jobCountText.match(/(\d+)/)[0]);
-        //     console.log(`Found ${jobCount} jobs:`);
-        // }
-
-        // Extract job data on current page
-        const currentPageJobs = await page.$$eval('#mosaic-provider-jobcards article', jobCards => {
-            return jobCards.map(jobCard => {
-                const titleEl = jobCard.querySelector('.jobtitle a');
-                const title = titleEl.textContent.trim();
-                const url = titleEl.href;
-                const company = jobCard.querySelector('.companyName').textContent.trim();
-                const location = jobCard.querySelector('.companyLocation').textContent.trim();
-                const date = jobCard.querySelector('.date').textContent.trim();
-                const description = jobCard.querySelector('.job-snippet').textContent.trim();
-                return { title, url, company, location, date, description };
-            });
+    const jobs = await page.evaluate(() => {
+        //fetch all the jobs according to queries;
+        const heading = document.querySelectorAll(
+            "#mosaic-provider-jobcards > ul > li"
+        );
+        let arr = [];
+        heading.forEach((ele) => {
+            let a = ele.innerText;
+            a = a.trim().split("\n");
+            let beg1 = "", beg2 = "";
+            for (let i = 4; i < a.length; i++) {
+                beg1 += a[i] + ".";
+            };
+            for (let i = 3; i < a.length; i++) {
+                beg2 += a[i] + ".";
+            };
+            let obj = {
+                title: a[0],
+                comapny_name: a[1] == "new" ? a[2] : a[1],
+                location: a[1] == "new" ? a[3] : a[2],
+                description: a[1] == "new" ? beg1 : beg2,
+                date: a[1] == 'new' ? beg1.slice(beg1.length - 18, beg1.length - 1) : beg2.slice(beg2.length - 19, beg2.length - 1)
+            };
+            arr.push(obj);
         });
-        jobs = [...jobs, ...currentPageJobs];
-
-        // Click "Next" button to go to next page
-        // const nextButton = await page.$('#pagination-buttons-container > div > a:last-child');
-        // const disabled = await nextButton.evaluate(node => node.classList.contains('disabled'));
-        // if (disabled) {
-        //     break;
-        // }
-        if (currentPageJobs.length >= 20) {
-            break;
-        }
-        await Promise.all([
-            page.waitForNavigation(),
-            // nextButton.click()
-        ]);
-        jobCount++;
-        console.log(jobCount);
-        console.log(currentPageJobs);
-    }
+        return arr;
+    });
 
     console.log(jobs);
 
+    // Save the extracted data in a structured JSON format as per the name mention;
+    fs.writeFile('db.json', JSON.stringify(jobs), (error) => {
+        if (error) {
+            console.log(error);
+            throw error;
+        } else {
+            console.log('Successfully File Saved in JSON Formate');
+        }
+    });
+
+    // automatically close browser after 5000ms;
     await browser.close();
-}
+};
+
 scrapeIndeed();
